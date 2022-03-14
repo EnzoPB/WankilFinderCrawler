02b0e7db4cdf6f1f0ae12e3eb65673425750a57c
Enzo <nelox@mail.com>
2022-03-14 14:19:18 +0100

    ajout des timestamps pour les sous-titres et de l'auteur pour les commentaires

--- crawler.py
+++ crawler.py
@@ -1,5 +1,6 @@
 from youtube_transcript_api import YouTubeTranscriptApi, _errors as YouTubeTranscriptApiErrors
 from datetime import datetime
+import json
 
 from database import con, cur # on importe la connexion et le curseur de la base de donnée
 from youtubeAPI import youtubeAPI # on importe la fonction youtubeAPI, qui sert juste à formatter des requêtes à l'API youtube
@@ -42,32 +43,47 @@ if __name__ == '__main__':
             })['items'] # 'items' correspond à la liste des commentaires
 
             for comment in comments:
-                topComments.append(comment['snippet']['topLevelComment']['snippet']['textDisplay']) # on ajoute le commentaire à la liste (uniquement le texte)
+                comment = comment['snippet']['topLevelComment']['snippet']
+                topComments.append([
+                    comment['authorDisplayName'],
+                    comment['textDisplay']
+                ]) # on ajoute le commentaire à la liste (uniquement l'auteur et le texte)
 
             try:
                 # on récupère les sous-titres français de la vidéo (automatiques et manuels) voir https://pypi.org/project/youtube-transcript-api
                 transcripts = YouTubeTranscriptApi.list_transcripts(videoId) # on récupère la liste des sous-titres pour la vidéos
             except YouTubeTranscriptApiErrors.TranscriptsDisabled: # les sous-titres sont désactivés pour cette vidéo
-                pass # on ignore l'erreur, un string vide "" sera stocké dans la bdd
+                pass # on ignore l'erreur, rien n'est ajouté dans l'array
 
             autoCaptionsList = [] # liste contenant les sous-titres automatiques
             try:
                 autoCaptions = transcripts.find_generated_transcript(['fr']).fetch() # on récupère les sous-titres automatiques en français
                 for caption in autoCaptions:
-                    autoCaptionsList.append(caption['text']) # on ajoute le texte du commentaire à la liste
+                    autoCaptionsList.append([
+                        caption['start'],
+                        caption['text']
+                    ]) # on ajoute le timestamp et texte du sous-titre à la liste
             except YouTubeTranscriptApiErrors.NoTranscriptFound: # les sous-titres sont désactivés / indisponibles
-                pass # on ignore l'erreur, un string vide "" sera stocké dans la bdd
+                pass # on ignore l'erreur, rien n'est ajouté dans l'array
 
             manualCaptionsList = [] # liste contenant les sous-titres manuels
             try:
                 manualCaptions = transcripts.find_manually_created_transcript(['fr']).fetch() # on récupère les sous-titres manuels en français
                 for caption in manualCaptions:
-                    manualCaptionsList.append(caption['text']) # on ajoute le texte du commentaire à la liste
+                    manualCaptionsList.append([
+                        caption['start'],
+                        caption['text']
+                    ]) # on ajoute le timestamp et texte du sous-titre à la liste
             except YouTubeTranscriptApiErrors.NoTranscriptFound: # les sous-titres sont indisponibles
-                pass # on ignore l'erreur, un string vide "" sera stocké dans la bdd
+                pass # on ignore l'erreur, rien n'est ajouté dans l'array
+
+            # on convertit les sous titres et les commentaires en JSON
+            manualCaptionsList = json.dumps(manualCaptionsList)
+            autoCaptionsList = json.dumps(autoCaptionsList)
+            topComments = json.dumps(topComments)
 
             # on créér une nouvelle ligne dans la bdd
             cur.execute('INSERT INTO videos(id, title, description, timestamp, topComments, autoCaptions, manualCaptions) VALUES(?, ?, ?, ?, ?, ?, ?)',
-                (videoId, video['title'], video['description'], timestamp, '\n'.join(topComments), '\n'.join(autoCaptionsList), '\n'.join(manualCaptionsList)))
+                (videoId, video['title'], video['description'], timestamp, topComments, autoCaptionsList, manualCaptionsList))
             con.commit()
 con.close()
